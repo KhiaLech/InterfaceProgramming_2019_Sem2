2a1b715172c54f76ec2e6d97718faf6eb103354d
KhiaLech <48078072+KhiaLech@users.noreply.github.com>
2019-11-11 12:27:02 +1100

    Displaying list

--- HTTP Posts/HTTP-Posts/src/app/app.module.ts
+++ HTTP Posts/HTTP-Posts/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
